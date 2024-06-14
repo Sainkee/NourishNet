6bdaa65329a66f8cc71bcc3577d2ceca7c941f12
Santosh kumar gupta <sainkee1997@gmail.com>
2024-06-14 15:20:25 +0530

    btn ui fixed

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -132,8 +132,9 @@ export default function Cart() {
           </p>
         </div>
         <button
+          disabled={total == 0}
           onClick={handlePayment}
-          className="mt-4 w-full whitespace-nowrap bg-green-500 text-white px-4 py-2 rounded-lg hover:bg-green-700 transition"
+          className="mt-4 w-full whitespace-nowrap disabled:bg-slate-600 disabled:cursor-not-allowed bg-green-500 text-white px-4 py-2 rounded-lg hover:bg-green-700 transition"
         >
           Proceed to Checkout
         </button>
